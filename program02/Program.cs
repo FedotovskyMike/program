9d9ce122b4fbd9f0f619162c911159182310226c
Fedotovsky M <fedotovsky.m@yandex.ru>
2022-06-12 13:48:58 +0300

    Домашнее задание

---
+++ program02/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Написать программу, которая принимает на вход трёхзначное
+число и на выходе показывает последнюю цифру этого числа */
+
+int n = int.Parse(Console.ReadLine());
+
+if(n > 99 & n < 1000)
+{
+  Console.WriteLine(n);
+  Console.WriteLine(n % 10);  
+}
+else
+{
+    Console.WriteLine("ОШИБКА");
+}
+
